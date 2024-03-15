414ea1da030e0ce924462a1531238eff7024c66e
shashanknv <shashanknv333@gmail.com>
2024-03-15 20:22:01 +0530

    asdfghjkl

--- 02_Basics/02Operators.js
+++ 02_Basics/02Operators.js
@@ -1,3 +1,6 @@
+// "use strict"  -----> used for clean and safe running
+
+
 var sellingPrice= 199;
 var listingPrice= 799;
 
@@ -6,3 +9,15 @@ var discountPercent = (( listingPrice - sellingPrice) / listingPrice) * 100;
 console.log("Discount percentage is : "+ discountPercent);
 displayDiscountPercent = Math.round(discountPercent);
 console.log("approx : "+displayDiscountPercent);
+
+
+// -------------> DATA TYPES <---------------
+let name = "shashank"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+console.log(typeof "shashank")
+console.log(typeof 20)
+console.log(typeof null)
+console.log(typeof undefined)
